6fa96c783e8dc4d975287899d3e3af1549bb96ad
sirosh <sirosh@iac.spb.ru>
2018-02-24 19:22:30 +0300

    Added React-Bootstrap Navbar and test for it

---
+++ src/MainNavbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+class MainNavbar extends Component {
+  render() {
+    return (
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Logo</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#about">
+              ABOUT
+            </NavItem>
+            <NavItem eventKey={2} href="#services">
+              SERVICES
+            </NavItem>
+            <NavItem eventKey={3} href="#pricing">
+              PRICING
+            </NavItem>
+            <NavItem eventKey={4} href="#contact">
+              CONTACT
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default MainNavbar;
