4a541350c8f3e2da346a0b5c08a80d2b2ede323b
N0tSure <ASir2089@gmail.com>
2018-03-26 01:42:05 +0300

    Validator implemented, using tests

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,5 @@
-const validationParams = [
-  {'name': 'error-name', 'type': 'string'},
+const constraintAttrs = [
+  {'name': 'error-name', 'type': 'string', 'nonEmpty': true},
   {'name': 'error-description', 'type': 'string'},
   {'name': 'error-detector', 'type': 'function'}
 ];
@@ -7,28 +7,61 @@ const validationParams = [
 class Validator {
   constructor(props) {
     this.fields = Object.keys(props);
-    this.validations = {};
+
+    if (!this.fields.length) {
+      throw new Error('Zero constraints was given');
+    }
+
+    this.constraints = {};
     this.fields.forEach((field) => {
+
       let constraint = props[field];
-      validationParams.forEach((param) => {
+
+      constraintAttrs.forEach((param) => {
         if (typeof(constraint[param['name']]) !== param['type']) {
           throw new TypeError(
             'property ' + param['name'] + ' must be ' + param['type']
           );
+        } else if (param['nonEmpty'] && !constraint[param['name']]) {
+          throw new TypeError(
+            'property ' + param['name'] + ' must be not empty string');
         }
       });
-      this.validations[field] = props[field];
+
+      this.constraints[field] = props[field];
     });
   }
 
-  checkConstraintProperties(constraint) {
-    validationParams.forEach((param) => {
-      if (typeof(constraint[param['name']]) !== param['type']) {
-        throw new TypeError(
-          'property ' + param['name'] + ' must be ' + param['type']
-        );
+  validate = (target) => {
+    const errors = {};
+    this.fields.forEach((field) => {
+      if (!this.constraints[field]['error-detector'](target[field])) {
+        errors[this.constraints[field]['error-name']] =
+          this.constraints[field]['error-description'];
       }
+
     });
+
+    errors.hasErrors = () => {
+      return this.hasErrors(errors);
+    };
+
+    errors.findError = (name) => {
+      return this.findError(name, errors);
+    }
+
+    return errors;
+  }
+
+
+  findError = (errorName, errors) => {
+    return errors[errorName] ? errors[errorName] : null;
+  }
+
+  hasErrors = (errors) => {
+    return Object.keys(errors)
+      .some(attrName => typeof(errors[attrName]) != 'function');
+
   }
 
 }
