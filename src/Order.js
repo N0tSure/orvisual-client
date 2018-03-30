e2239c40ee98c6ef7a6ebe3e079572584b6eaebb
N0tSure <ASir2089@gmail.com>
2018-03-30 19:01:53 +0300

    Added logic for processing data

--- src/Order.js
+++ src/Order.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {
+  Alert,
   Grid, Row, Col, Glyphicon,
   Form, HelpBlock,
   FormGroup, ControlLabel, FormControl, InputGroup,
   Button, ButtonToolbar
 } from 'react-bootstrap';
 import OrderForm from './fragments/OrderForm';
+import { Link } from "react-router-dom";
+
+const initialState = 'initial-state';
+const successState = 'success-state';
+const failedState = 'failed-state';
 
 class Order extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      control: props.match.params.control
+    };
   }
 
   processOrderData = (order, files) => {
@@ -21,15 +29,49 @@ class Order extends React.Component {
   }
 
   render() {
+    let funcBlock = null;
+    switch (this.state.control) {
+      case 'success':
+        funcBlock = <SuccessfulOrderSubmission />;
+        break;
+      case 'failed':
+        funcBlock = <FailedOrderSubmission url={this.props.match.url}/>
+        break;
+      default:
+        funcBlock = <OrderForm sendFormData={this.processOrderData} />;
+    }
     return(
-
-      <OrderForm sendFormData={(order, files) => {
-        console.log(JSON.stringify(order));
-        console.log(files.toString());
-      }} />
-
+      <Grid fluid>
+        {funcBlock}
+      </Grid>
     );
   }
 }
+
+const FailedOrderSubmission = (props) => {
+  return(
+    <Alert bsStyle="danger">
+      <h2>Oops! Something going wrong!</h2>
+      <p>
+        Looks like order submission failed. You can repeat form filling, or go
+        back on home page.
+      </p>
+      <p>
+        <Link to={`${props.url}/order`}>Try again</Link>
+        <span> or </span>
+        <Link to="/">Go Back</Link>
+      </p>
+    </Alert>
+  );
+};
+
+const SuccessfulOrderSubmission = () => {
+  return(
+    <Alert bsStyle="success">
+      <h2>Submittied!</h2>
+      <p>Order has been successfully submit. We contact you soon for details.</p>
+    </Alert>
+  );
+};
 
 export default Order;
--- src/Order.test.js
+++ src/Order.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Order from './Order';
+import createHistory from "history/createBrowserHistory";
+import { MemoryRouter, Route, Switch } from 'react-router-dom';
 
 it('Order page should renders without crashes', () => {
   const root = document.createElement('div');
-  ReactDOM.render(<Order />, root);
+  ReactDOM.render(<Order match={ { params: { control: '' } }} />, root);
+  ReactDOM.unmountComponentAtNode(root);
+})
+
+it('Should inform about successfull order submission', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Order match={{ params: { control: 'success' } }} />, root);
   ReactDOM.unmountComponentAtNode(root);
 })
