b894c1592794ddc9792a76b3c1f348b4e7e39c0c
N0tSure <ASir2089@gmail.com>
2018-04-05 19:56:02 +0300

    Logic for render pictures changed, Picture moved to another function, added styles

--- src/fragments/PicturesPane.js
+++ src/fragments/PicturesPane.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row, Col, Image, Button } from 'react-bootstrap';
+import './Picture.css';
 
 const PicturesPane = (props) => {
   let rows = [];
@@ -7,17 +8,24 @@ const PicturesPane = (props) => {
 
   props.images.forEach((image, index) => {
     let rowNum = Math.floor(index / 2);
-    if (rows[rowNum] === undefined)
-      rows[rowNum] = [];
-
+    if (rows[rowNum] === undefined) {
+      rows[rowNum] = [
+        <Picture
+          key={index}
+          fileIndex={index}
+          columnOffset={2}
+          image={image} removePicture={props.removePicture} />
+      ];
+    } else {
       rows[rowNum].push(
-        <Col key={index} sm={4} smOffset={2}>
-          <Image
-            key={image.name}
-            src={window.URL.createObjectURL(image)} thumbnail />
-        </Col>
+        <Picture
+          key={index}
+          fileIndex={index}
+          columnOffset={0} image={image}
+          removePicture={props.removePicture}
+         />
       );
-
+    }
   });
 
   layout = rows.map((row, index) => {
@@ -30,6 +38,18 @@ const PicturesPane = (props) => {
     <React.Fragment>
       {layout}
     </React.Fragment>
+  );
+}
+
+const Picture = (props) => {
+  return(
+    <Col className="picture-block" sm={4} smOffset={props.columnOffset}>
+      <Image
+        key={props.image.name}
+        alt={props.image.name}
+        src={window.URL.createObjectURL(props.image)} responsive />
+      <Button bsSize="small" bsStyle="danger" onClick={() => props.removePicture(props.fileIndex)}>x</Button>
+    </Col>
   );
 }
 
