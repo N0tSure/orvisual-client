bf8d8fe4cd2e76c63734c4b689d24679bf053f4a
N0tSure <ASir2089@gmail.com>
2018-07-09 23:13:02 +0300

    Sort for timestamps implemented

--- src/Utils.js
+++ src/Utils.js
@@ -1,37 +1,55 @@
 import namor from "namor";
 import moment from 'moment';
-
-const range = len => {
-  const result = new Array(len);
-  for (let i = 0; i < len; i++) {
-    result[i] = i;
-  }
-
-  return result;
-};
+import _ from 'lodash';
 
 const Order = () => {
-  const accepted = Math.random() < 0.01 ? null : moment()
-    .subtract(Math.ceil(Math.random() * 1000), 'day');
+  const accepted = Math.random() > 0.5 ? null : moment()
+    .subtract(Math.ceil(Math.random() * 1000), 'day').format(moment.defaultFormatUtc);
 
-  let completed = null;
-  if (accepted) {
-    completed = Math.random() > 0.9 ? null : accepted.clone()
-      .subtract(Math.ceil(Math.random() * 10), 'day').format(moment.defaultFormatUtc);
-  } else {
-    completed = null;
-  }
+  const completed = Math.random() > 0.5 ? null : moment()
+    .subtract(Math.ceil(Math.random() * 1000), 'day').format(moment.defaultFormatUtc);
 
   return {
     'clientName': namor.generate({ words: 2, numbers: 0}),
     'clientPhone': Math.ceil(Math.random() * 10000000).toString(),
     'clientEmail': `${namor.generate({ words: 1, numbers: 0})}@mail.com`,
-    'acceptedAt': accepted ? accepted.format(moment.defaultFormatUtc) : null,
+    'acceptedAt': accepted,
     'completedAt': completed,
     'description': namor.generate({ words: 4, numbers: 0})
   };
 };
 
+const transformStatusColumnSort = sorted => {
+  const columnIndex = sorted.findIndex(sort => sort.id === 'orderStatus');
+  if (columnIndex >= 0) {
+    const copiedSorted = Object.assign([], sorted);
+    const sort = copiedSorted[columnIndex];
+    copiedSorted.splice(columnIndex, 1, { id: 'acceptedAt', desc: sort.desc });
+    copiedSorted.splice(columnIndex, 0, { id: 'completedAt', desc: sort.desc});
+
+    return copiedSorted;
+  }
+
+  return sorted;
+};
+
+export function sortData(data, sorted) {
+  const sortingOrder = transformStatusColumnSort(sorted);
+  return _.orderBy(
+    data,
+    sortingOrder.map(so => {
+      return row => {
+        if ((so.id === 'acceptedAt' || so.id === 'completedAt') && !row[so.id]) {
+          return '1970-01-01T00:00:00Z'
+        }
+
+        return typeof row[so.id] === "string" ? row[so.id].toLowerCase() : row[so.id];
+      };
+    }),
+    sortingOrder.map(so => so.desc ? 'desc' : 'asc')
+  );
+}
+
 export function makeData(len = 5000) {
-  return range(len).map(d => Order());
+  return _.range(len).map(d => Order());
 }
