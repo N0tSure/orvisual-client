34843876c2528d88b327d3ee7b4c223b663f52c3
N0tSure <ASir2089@gmail.com>
2018-07-12 19:49:31 +0300

    Test error fixed, code clean up and docs added

--- src/Table.test.js
+++ src/Table.test.js
@@ -4,6 +4,7 @@ import Table from './Table';
 import jest from 'jest-mock';
 
 it('should render without crashing', () => {
+
   window.URL = function URL(url, base) {
     this.base = base;
     this.url = url;
@@ -11,8 +12,27 @@ it('should render without crashing', () => {
       append: (name, value) => `${name}=${value}`
     };
   };
-  window.fetch = jest.fn(() => new Promise((resolve, reject) => resolve()));
+  const dummy_response = {
+    ok: true,
+    json: function() {
+      return {
+        '_embedded': {
+          'orders': []
+        },
+        'page': {
+          'totalPages': 0
+        }
+      };
+    }
+  };
+
+  const dummy_fetch = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve(dummy_response), 500);
+  });
+
+  window.fetch = jest.fn(() => dummy_fetch);
   const root = document.createElement('div');
   ReactDOM.render(<Table />, root);
   ReactDOM.unmountComponentAtNode(root);
+  
 });
