a9760fa44eaacea11c6a32b2a4dfcdcb6b816707
N0tSure <ASir2089@gmail.com>
2018-07-14 23:00:09 +0300

    Add mocked fetch in test
    Mock fetch function assigned to window to prevent network request. Test 
    RowInfo added too.

--- src/fragments/OrderRow.test.js
+++ src/fragments/OrderRow.test.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import OrderRow from './OrderRow';
 
 it('should render without crashing', () => {
+  const realFetch = window.fetch;
+  window.fetch = () => new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(), 500);
+  });
   const root = document.createElement('div');
-  ReactDOM.render(<OrderRow />, root);
+  const rowInfo = {
+    original: {
+      '_links': {
+        pictures: {
+          href: 'http://localhost/api/pictures'
+        }
+      }
+    }
+  };
+
+  ReactDOM.render(<OrderRow rowInfo={rowInfo} />, root);
   ReactDOM.unmountComponentAtNode(root);
+  window.fetch = realFetch;
 })
